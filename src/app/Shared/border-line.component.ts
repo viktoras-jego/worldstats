e2125a3e750f7b847e28e23e2f60495800ee99c8
Viktoras Jegorovas <viktoras.jego@gmail.com>
2017-07-27 16:39:50 +0300

    boxing

--- src/app/Shared/border-line.component.ts
+++ src/app/Shared/border-line.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'border-line',
     template: `
-        <svg style="width: 100vw; height: 5px">
-            <line
-                  x1="7%" x2="93%" y1="5" y2="5" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-dasharray=".15, 17">
+        <svg style="width: 100vw; height: 8px">
+            <line x1="7%" x2="93%" y1="5" y2="5" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-dasharray=".15, 17">
             </line>
         </svg>
     `
--- src/app/Shared/bot-border-line.component.ts
+++ src/app/Shared/bot-border-line.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'bot-border-line',
     template: `
-        <svg style="position: absolute; bottom: 0;transform: translate(-50%, 0); width: 100%" height="5px">
+        <svg style="position: absolute; bottom: 0;transform: translate(-50%, 0); width: 100%" height="8px">
             <line x1="7%" x2="93%" y1="5" y2="5" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-dasharray=".15, 17">
             </line>
         </svg>
