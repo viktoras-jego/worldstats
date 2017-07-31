0c26d693ced52de2f0cf0132aa93a56989123181
Viktoras Jegorovas <viktoras.jego@gmail.com>
2017-07-31 16:35:49 +0300

    Third page completed

--- src/app/Pages/Third page/third-page.component.ts
+++ src/app/Pages/Third page/third-page.component.ts
@@ -15,9 +15,49 @@ import { CountoModule } from 'angular2-counto';
 })
 export class ThirdPageComponent implements AfterViewInit {
 
+    public seconds: number = 0;
+    public minutes: number = 0;
+    public duration = 0.3;
+
+    public goldWasteFrom = 0;
+    public energyFrom = 0;
+    public steelFrom = 0;
+
+    public goldWastePerSecond = 236.808726;
+    public energyPerSecond = 1.642134;
+    public steelPerSecond = 2853.8812;
+
+    public goldWasteTo = this.goldWastePerSecond;
+    public energyTo = this.energyPerSecond;
+    public steelTo = this.steelPerSecond;
+
+    public timer = 0;
+
     constructor(private a: MnFullpageService,
-                private cdRef: ChangeDetectorRef) {}
+                private cdRef: ChangeDetectorRef,
+                public counto: CountoModule,
+                public counto2: CountoModule,
+                public counto3: CountoModule,
+                public counto4: CountoModule) {}
 
     ngAfterViewInit(): void {
+    }
+
+    goDown(): void {
+        this.a.moveSectionDown();
+    }
+    changeCounter(): void {
+        setTimeout(() => {
+            this.timer ++;
+            this.seconds = this.timer % 60;
+            this.minutes = Math.floor(this.timer / 60);
+
+            this.goldWasteFrom = this.goldWasteTo;
+            this.goldWasteTo += this.goldWastePerSecond;
+            this.energyFrom = this.energyTo;
+            this.energyTo += this.energyPerSecond;
+            this.steelFrom = this.steelTo;
+            this.steelTo += this.steelPerSecond;
+        }, 670);
     }
 }
